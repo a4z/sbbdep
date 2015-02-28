cddb88972eea1d138d81f7d2b7675d4c2fa3dd93
a4z <harald.achitz@gmail.com>
2015-02-28 17:43:09 +0100

    fix for pkg build

--- mksrcdists.sh
+++ mksrcdists.sh
@@ -5,19 +5,21 @@ version=$(grep -i SBBDEP_MAJOR_VERSION include/sbbdep/config.hpp | tr -d -c '[0-
 $(grep -i SBBDEP_MINOR_VERSION include/sbbdep/config.hpp | tr -d -c '[0-9]').\
 $(grep -i SBBDEP_PATCH_VERSION include/sbbdep/config.hpp | tr -d -c '[0-9]')
 
-
-outname=sbbdep-${version}.tar.gz
+#define tmp dir and archive name
+tmpdir=/tmp
+outname=../sbbdep-${version}.tar.gz
 
 #remove possible existings 
-rm ${outname} 
-rm /tmp/${outname} 
- 
-# TODO check this shoudl handle metadata, not_ .?* 
-tar -C ../ \
+rm -rf ${outname} ${tmpdir}/sbbdep-${version}
+
+#copy wanted stuff into versioned dir in tmp 
+rsync -a \
 --exclude=*~ \
 --exclude=.?* \
 --exclude=build* \
--cvzf /tmp/${outname} sbbdep   
- 
-mv /tmp/${outname} ./
+ $(pwd)/*  ${tmpdir}/sbbdep-${version} \
+
+ #pack the stuff 
+tar -C ${tmpdir} -cvzf ${outname} sbbdep-${version}
+
 
