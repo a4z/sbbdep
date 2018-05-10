7688636cd02ab011faaa8c764d56e1461e714b8c
a4z <harald.achitz@gmail.com>
2018-05-10 18:56:40 +0200

    enable ccache for development

--- CMake/setup_compiler.cmake
+++ CMake/setup_compiler.cmake
@@ -36,7 +36,21 @@ if (NOT have_myCompilerSetup)
             add_definitions(--coverage -fprofile-arcs -ftest-coverage)
             set (OPTION_GCOVLIB gcov CACHE STRING INTERNAL)
         endif(CODECOVERAGE)
-        
+
+        if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+            find_program(CCACHE_BIN ccache)
+            if(CCACHE_BIN)
+                set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ${CCACHE_BIN})
+                set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ${CCACHE_BIN})
+
+                # ccache uses -I when compiling without preprocessor, which makes clang complain.
+                if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+                    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Qunused-arguments -fcolor-diagnostics")
+                    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Qunused-arguments -fcolor-diagnostics")
+                endif()
+            endif()
+        endif (CMAKE_BUILD_TYPE STREQUAL "Debug")
+
     elseif( MSVC )
     # TODO default flags ? 
     
